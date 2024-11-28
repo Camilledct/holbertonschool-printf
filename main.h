068b68aa0f9bab3f2ed33b0ce33a444fe942d547
Camilledct <camilledct28@gmail.com>
2024-11-28 15:16:04 +0100

    fix main.h

--- main.h
+++ main.h
@@ -4,8 +4,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h> 
+#include <stdlib.h> 
 
 int _printf(const char *format, ...);
 int _putchar(char c);
